4d9cbac67a73b35217310548124bd896ad2e1826
lucas-burdell <lucasburdell@gmail.com>
2019-01-05 14:30:18 -0600

    fix player

--- Player.js
+++ Player.js
@@ -1,29 +1,21 @@
 import Vector2 from "./Vector2";
+import Entity from "./Entity";
+import { AddEventCallback, CONTROLS } from "./Controls";
 
-export default class Player {
+export default class Player extends Entity {
   constructor(image, x, y) {
-    this.image = image;
-    this.position = new Vector2(x, y);
+    super(image, new Vector2(x, y));
     AddEventCallback(CONTROLS.LEFT, () => {
       player1.position = player1.position.add(new Vector2(-5, 0));
-      player1.update();
     });
     AddEventCallback(CONTROLS.RIGHT, () => {
       player1.position = player1.position.add(new Vector2(5, 0));
-      player1.update();
     });
     AddEventCallback(CONTROLS.UP, () => {
       player1.position = player1.position.add(new Vector2(0, -5));
-      player1.update();
     });
     AddEventCallback(CONTROLS.DOWN, () => {
       player1.position = player1.position.add(new Vector2(0, 5));
-      player1.update();
     });
-  }
-
-  update(canvas) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(this.image, this.position.x, this.position.y);
   }
 }
