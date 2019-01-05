50b58900c2a99e416ece6dfeb4532f63ed894a10
Sperges <rnbiv45@gmail.com>
2019-01-05 15:31:16 -0500

    yes

--- Vector2.js
+++ Vector2.js
@@ -3,6 +3,44 @@ export default class Vector2 {
     this.x = x;
     this.y = y;
   }
+  
+  static down() {
+    return new Vector2(0, -1);
+  }
+
+  static left() {
+    return new Vector2(-1, 0);
+  }
+
+  static one() {
+    return new Vector2(1, 1);
+  }
+
+  static right() {
+    return new Vector2(1, 0);
+  }
+
+  static up() {
+    return new Vector2(0, 1);
+  }
+
+  static zero() {
+    return new Vector2(0, 0);
+  }
+
+  magnitude() {
+    return Math.sqrt((this.x * this.x) + (this.y * this.y));
+  }
+
+  normalize() {
+    let vector = Vector2.zero();
+    let length = this.magnitude();
+    if (length != 0) {
+      vector.x = this.x / length;
+      vector.y = this.y / length;
+    }
+    return vector
+  }
 
   add(other) {
     if (other instanceof Vector2) {
@@ -19,4 +57,9 @@ export default class Vector2 {
       throw new Error(other + " is not a valid Vector2");
     }
   }
+
+  toString() {
+    return this.x + ", " + this.y;
+  }
+
 }
