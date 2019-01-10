660814fddecf284adfa65cae17a1abec3a62e6dd
lucas-burdell <lucasburdell@gmail.com>
2019-01-09 20:18:28 -0600

    add pipeline as global variable

--- Game/Player.js
+++ Game/Player.js
@@ -3,6 +3,7 @@ import Entity from "Engine/Entity";
 import ControlBindings from "Engine/ControlBindings";
 import BoundingBox from "Engine/BoundingBox";
 import Projectile from "./Projectile";
+import Pipeline from "Engine/EntityPipeline";
 
 export default class Player extends Entity {
   /**
@@ -11,14 +12,9 @@ export default class Player extends Entity {
    * @param {Vector2} size
    */
   constructor(image, position, size) {
-    super(
-      image,
-      position,
-      new BoundingBox(position, size)
-    );
+    super(image, position, new BoundingBox(position, size));
 
     this.bullet;
-    this.pipeline;
 
     let that = this;
     ControlBindings.AddControlEvent("LEFT", () => {
@@ -27,21 +23,23 @@ export default class Player extends Entity {
     ControlBindings.AddControlEvent("RIGHT", () => {
       that.velocity = that.velocity.add(new Vector2(0.1, 0));
     });
-    //ControlBindings.AddControlEvent("UP", () => {
-    //  that.velocity = that.velocity.add(new Vector2(0, -0.001));
-    //});
-    //ControlBindings.AddControlEvent("DOWN", () => {
-    //  that.velocity = that.velocity.add(new Vector2(0, 0.001));
-    //});
     ControlBindings.AddControlEvent("FIRE", () => {
-      let bullet = new Projectile(this.bullet, this.position.add(new Vector2(8,32)), new Vector2(16,16));
+      let bullet = new Projectile(
+        this.bullet,
+        this.position.add(new Vector2(8, 32)),
+        new Vector2(16, 16)
+      );
       bullet.usesVelocity = true;
-      bullet.velocity = new Vector2(0,-1);
-      this.pipeline.AddEntity(bullet);
-      let bullet2 = new Projectile(this.bullet, this.position.add(new Vector2(64-24,32)), new Vector2(16,16));
+      bullet.velocity = new Vector2(0, -1);
+      Pipeline().AddEntity(bullet);
+      let bullet2 = new Projectile(
+        this.bullet,
+        this.position.add(new Vector2(64 - 24, 32)),
+        new Vector2(16, 16)
+      );
       bullet2.usesVelocity = true;
-      bullet2.velocity = new Vector2(0,-1);
-      this.pipeline.AddEntity(bullet2);
+      bullet2.velocity = new Vector2(0, -1);
+      Pipeline().AddEntity(bullet2);
     });
   }
 
